a7f718b12aa3decbf0cdacf7eed2bcf0a869eaaa
Matteo Depalo <matteodepalo@gmail.com>
2013-05-24 13:11:00 +0200

    Refactored test helpers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,9 +1,12 @@
 module Test::Helpers
   ['get', 'post', 'patch', 'delete', 'put'].each do |method|
-    define_method "#{method}_with_auth" do |path, data = nil, user: nil|
-      send(method, path, data.to_json, {
-        authorization: ActionController::HttpAuthentication::Token.encode_credentials(user.api_keys.first_or_create.access_token),
-        CONTENT_TYPE: 'application/json'
+    define_method method do |path, data = {}, headers = {}|
+      super(path, data.to_json, headers.merge(CONTENT_TYPE: 'application/json'))
+    end
+
+    define_method "#{method}_with_auth" do |path, data = {}, user: nil|
+      send(method, path, data, {
+        authorization: ActionController::HttpAuthentication::Token.encode_credentials(user.api_keys.first_or_create.access_token)
       })
     end
   end
