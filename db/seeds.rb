146fd6dab07ef36f5e0f76e0bcd6859c9cd8003f
Matteo Depalo <matteodepalo@gmail.com>
2013-05-31 09:53:31 -0400

    Added number of teams to games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-dota2 = Game.build_and_create(name: :dota2)
-go = Game.build_and_create(name: :go)
-table_football = Game.build_and_create(name: :table_football)
-
-test_user = User.create(name: 'test')
-participant = User.create(name: 'participant')
-round = Round.create(user: test_user, game: go)
-Participation.create(user: participant, round: round)
+Game::VALID_GAME_NAMES.each do |game_name|
+  Game.build_and_create(name: game)
+end
