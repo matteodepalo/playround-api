614c9246f470472a2d0884d84b053e4a04e31187
Eugenio Depalo <eugeniodepalo@gmail.com>
2013-06-01 12:55:58 +0200

    Wrap token creation in a hash.

--- app/controllers/v1/tokens_controller.rb
+++ app/controllers/v1/tokens_controller.rb
@@ -1,6 +1,6 @@
 class V1::TokensController < ApplicationController
   def create
-    graph = Koala::Facebook::API.new(params[:facebook_access_token])
+    graph = Koala::Facebook::API.new(params[:token][:facebook_access_token])
     info = graph.get_object('me')
     @user = User.find_or_create_by_facebook_oauth(info)
 
