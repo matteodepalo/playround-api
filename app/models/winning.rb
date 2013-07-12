e081aa2d88cb8fed076b2ec573ab2ee8e89e40e2
Matteo Depalo <matteodepalo@gmail.com>
2013-07-12 08:00:58 +0200

    Removed useless winning module inclusion

--- app/models/winning.rb
+++ app/models/winning.rb
@@ -1,17 +1,11 @@
 class Winning
   include ActiveModel::Model
-  include ActiveModel::Validations
 
   validates :round, presence: true
   validate :round_must_be_ongoing
   validate :team_name_must_be_among_valid_ones
 
-  attr_reader :round, :team_name
-
-  def initialize(round: nil, team_name: nil)
-    @round = round
-    @team_name = team_name
-  end
+  attr_accessor :round, :team_name
 
   def save
     if valid?
