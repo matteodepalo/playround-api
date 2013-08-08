4aa70c59bba4dc2af68c31b79832b144968d07e2
Matteo Depalo <matteodepalo@gmail.com>
2013-08-08 20:32:07 +0200

    Postgres is now also in production with chef

--- chef/site-cookbooks/main/recipes/setup.rb
+++ chef/site-cookbooks/main/recipes/setup.rb
@@ -29,6 +29,12 @@ if node[:environment] == 'production'
   bash 'reload ssh' do
     code 'reload ssh'
   end
+else
+  rbenv_gem 'spring'
+
+  magic_shell_environment 'SPRING_TMP_PATH' do
+    value '/tmp/spring'
+  end
 end
 
 directory "#{node[:sockets_folder]}" do
@@ -41,46 +47,38 @@ directory "#{node[:deploy_to]}/shared" do
   recursive true
 end
 
-if node[:environment] == 'development'
-  apt_repository 'ppa_sharpie_for-science' do
-    uri 'http://ppa.launchpad.net/sharpie/for-science/ubuntu'
-    distribution node['lsb']['codename']
-    components ["main"]
-    keyserver 'keyserver.ubuntu.com'
-    key 'DAF764E2'
-    deb_src true
-  end
+apt_repository 'ppa_sharpie_for-science' do
+  uri 'http://ppa.launchpad.net/sharpie/for-science/ubuntu'
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver 'keyserver.ubuntu.com'
+  key 'DAF764E2'
+  deb_src true
+end
 
-  apt_repository 'ppa_sharpie_postgis-stable' do
-    uri 'http://ppa.launchpad.net/sharpie/postgis-stable/ubuntu'
-    distribution node['lsb']['codename']
-    components ['main']
-    keyserver 'keyserver.ubuntu.com'
-    key 'DAF764E2'
-    deb_src true
-  end
+apt_repository 'ppa_sharpie_postgis-stable' do
+  uri 'http://ppa.launchpad.net/sharpie/postgis-stable/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key 'DAF764E2'
+  deb_src true
+end
 
-  apt_repository 'ppa_ubuntugis_ubuntugis-unstable' do
-    uri 'http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu'
-    distribution node['lsb']['codename']
-    components ['main']
-    keyserver 'keyserver.ubuntu.com'
-    key '314DF160'
-    deb_src true
-    cache_rebuild true
-  end
+apt_repository 'ppa_ubuntugis_ubuntugis-unstable' do
+  uri 'http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key '314DF160'
+  deb_src true
+  cache_rebuild true
+end
 
-  package 'python-software-properties'
-  package 'postgresql-9.2-postgis'
+package 'python-software-properties'
+package 'postgresql-9.2-postgis'
 
-  pg_user node[:db_user][:name] do
-    privileges :superuser => true, :createdb => true, :login => true
-    password node[:db_user][:password]
-  end
-
-  rbenv_gem 'spring'
-
-  magic_shell_environment 'SPRING_TMP_PATH' do
-    value '/tmp/spring'
-  end
+pg_user node[:db_user][:name] do
+  privileges :superuser => true, :createdb => true, :login => true
+  password node[:db_user][:password]
 end
